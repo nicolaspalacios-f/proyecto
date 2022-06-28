d7ac767ec3440cde42225e13c4ed0a7d6a81c607
nicolaspalacios-f <nicolas.palacios-f@mail.escuelaing.edu.co>
2022-06-28 11:58:40 -0500

    Formato carpetas

---
+++ src/main/java/edu/escuelaing/arsw/ASE/app/WebSiteController.java
@@ -0,0 +1,45 @@
+package edu.escuelaing.arsw.ASE.app;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@ComponentScan(basePackages = { "edu.escuelaing.arsw.ASE.app" })
+public class WebSiteController {
+    @Resource
+    private HttpServletRequest request;
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebSiteController.class, args);
+    }
+
+    @GetMapping("/status")
+    public String status() {
+        sessionManagement();
+        String name = (String) request.getSession().getAttribute("name");
+        return "{\"status\":\"Greetings from Spring Boot "
+                + name + ". " + java.time.LocalDate.now() + ", "
+                + java.time.LocalTime.now()
+                + ". " + "The server is Runnig!\"}";
+    }
+
+    @GetMapping("/setname")
+    public String setName(@RequestParam(value = "name", defaultValue = "Anónimo") String name) {
+        System.out.println(name);
+        request.getSession().setAttribute("name", name);
+        return String.format("Hello %s!", name);
+    }
+
+    public void sessionManagement() {
+        System.out.println(request.getSession(true).getId());
+    }
+}
