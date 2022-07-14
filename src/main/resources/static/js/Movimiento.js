66803a4572b6aff9fef38eac925bbd8bf0a54bb9
nicolaspalacios-f <nicolas.palacios-f@mail.escuelaing.edu.co>
2022-07-14 09:32:51 -0500

    push pal profe

--- src/main/resources/static/js/Movimiento.js
+++ src/main/resources/static/js/Movimiento.js
@@ -1,6 +1,6 @@
 const postits = document.querySelectorAll(".postit");
 const todos = document.querySelectorAll(".column");
-var crear = true;
+var propietario = sessionStorage.getItem("User");
 var datos = null;
 let move = null;
 postits.forEach((postit) => {
@@ -40,7 +40,7 @@ function drop() {
   var json = {
     divHtml: datos,
     column: move.getAttribute("columna"),
-    textopost: input_val,
+    accion: 1,
   };
   sincro(json);
   move = null;
@@ -76,9 +76,8 @@ window.onclick = (event) => {
 const todo_submit = document.getElementById("todo_submit");
 
 todo_submit.addEventListener("click", createTodo);
-
+var id = 0;
 function createTodo() {
-  crear = true;
   const todo_div = document.createElement("div");
   const input_val = document.getElementById("todo_input").value;
   const txt = document.createTextNode(input_val);
@@ -88,16 +87,20 @@ function createTodo() {
   todo_div.classList.add("postit");
   todo_div.setAttribute("draggable", "true");
   todo_div.setAttribute("columna", "columna_1");
+  todo_div.setAttribute("id", id);
+  id++;
 
   /* create span */
   const span = document.createElement("span");
   const span_txt = document.createTextNode("\u00D7");
   span.classList.add("close");
   span.appendChild(span_txt);
+  span.setAttribute("className", "acaa");
 
   todo_div.appendChild(span);
   columna_1.appendChild(todo_div);
   span.addEventListener("click", () => {
+    console.log(span);
     span.parentElement.style.display = "none";
   });
 
@@ -113,6 +116,9 @@ function createTodo() {
     divHtml: datos,
     column: todo_div.getAttribute("columna"),
     textopost: input_val,
+    idpostit: todo_div.getAttribute("id"),
+    propi: propietario,
+    accion: 0,
   };
   sincro(json);
 }
@@ -133,8 +139,7 @@ function setup() {
 var colores = "#" + Math.floor(Math.random() * 16777215).toString(16);
 
 function sincro(json) {
-  console.log(json.column, json.textopost, json.crear);
-  crear = false;
+  console.log(json.column, json.textopost, json.propi, json.accion);
   stompClient.send("/topic/kanvan", {}, JSON.stringify(json));
 }
 
@@ -146,39 +151,46 @@ function stomp() {
     stompClient.subscribe("/topic/kanvan", function (event) {
       var json = JSON.parse(event.body);
 
-      if (crear) {
-        const todo_div = document.createElement("div");
-        const txt = document.createTextNode(json.textopost);
-
-        todo_div.appendChild(txt);
-        todo_div.classList.add("post-it");
-        todo_div.classList.add("postit");
-        todo_div.setAttribute("draggable", "true");
-        todo_div.setAttribute("columna", json.column);
-
-        /* create span */
-        const span = document.createElement("span");
-        const span_txt = document.createTextNode("\u00D7");
-        span.classList.add("close");
-        span.appendChild(span_txt);
-
-        todo_div.appendChild(span);
-        columna_1.appendChild(todo_div);
-        span.addEventListener("click", () => {
-          span.parentElement.style.display = "none";
-        });
-
-        todo_div.addEventListener("dragstart", dragStart);
-        todo_div.addEventListener("dragend", dragEnd);
-
-        document.getElementById("todo_input").value = "";
-        todo_form.classList.remove("active");
-        overlay.classList.remove("active");
-      } else {
-        crear = true;
+      if (json.propi != propietario) {
+        switch (json.accion) {
+          case 0:
+            crearRecibido(json);
+            break;
+          case 1:
+            console.log("mover");
+        }
       }
     });
   });
+}
+function crearRecibido(json) {
+  const todo_div = document.createElement("div");
+  const txt = document.createTextNode(json.textopost);
+
+  todo_div.appendChild(txt);
+  todo_div.classList.add("post-it");
+  todo_div.classList.add("postit");
+  todo_div.setAttribute("draggable", "true");
+  todo_div.setAttribute("columna", json.column);
+  todo_div.setAttribute("id", json.idpostit);
+  /* create span */
+  const span = document.createElement("span");
+  const span_txt = document.createTextNode("\u00D7");
+  span.classList.add("close");
+  span.appendChild(span_txt);
+
+  todo_div.appendChild(span);
+  columna_1.appendChild(todo_div);
+  span.addEventListener("click", () => {
+    span.parentElement.style.display = "none";
+  });
+
+  todo_div.addEventListener("dragstart", dragStart);
+  todo_div.addEventListener("dragend", dragEnd);
+
+  document.getElementById("todo_input").value = "";
+  todo_form.classList.remove("active");
+  overlay.classList.remove("active");
 }
 setup();
 function message(json) {
--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,11 +1,15 @@
 var usuario = "admin";
 var contrasena = "admin";
+var usuario2 = "user";
+var contrasena2 = "user";
 sessionStorage.setItem("loggeado", false);
 
 function changeUrl() {
   if (
-    usuario === document.getElementById("Usuario").value &&
-    contrasena === document.getElementById("Password").value
+    (usuario === document.getElementById("Usuario").value &&
+      contrasena === document.getElementById("Password").value) ||
+    (usuario2 === document.getElementById("Usuario").value &&
+      contrasena2 === document.getElementById("Password").value)
   ) {
     sessionStorage.setItem("loggeado", true);
     sessionStorage.setItem("User", document.getElementById("Usuario").value);
